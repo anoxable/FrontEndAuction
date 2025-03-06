8847ba19014cc767a5e3afbb54ba491449fbf3ae
anoxable <43794757+anoxable@users.noreply.github.com>
2025-03-06 17:16:51 +0100

    Update page.tsx

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -32,7 +32,7 @@ export default function RegisterPage() {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://localhost:3001/users/signup', {
+      const response = await fetch('https://auctionbaybackend.onrender.com/users/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
