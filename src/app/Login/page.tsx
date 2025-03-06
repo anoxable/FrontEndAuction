a9732909f9c92b2bc4971bdd5d3f2ef2b69fd414
anoxable <43794757+anoxable@users.noreply.github.com>
2025-03-06 17:16:24 +0100

    Update page.tsx

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://localhost:3001/auth/login', {
+      const response = await fetch('https://auctionbaybackend.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
